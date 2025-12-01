f0c5cdfc75a8d76a450cf7d04f09867d80a44a25
Syed Rehan <jrehans@jci.com>
2025-12-01 17:10:55 +0530

    Removed un-used files

--- src/fixtures/shared-fixtures.ts
+++
@@ -1,6 +0,0 @@
-import { mergeTests } from '@playwright/test';
-import { test as popups } from './pop-ups-fixtures';
-import { test as navbar } from './navbar-fixtures';
-import { test as cards } from './cards-fixtures';
-
-export const test = mergeTests(popups, navbar, cards);
