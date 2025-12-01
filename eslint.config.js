eb473c2a609a48a0cb9fd0698fb1cfe957566354
Syed Rehan <jrehans@jci.com>
2025-12-01 17:48:23 +0530

    Remove react related configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
+
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 import i from 'eslint-plugin-import';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import parser from '@typescript-eslint/parser';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
@@ -15,23 +14,16 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
       eslintPluginPrettierRecommended,
     ],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.node,
       parser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       import: i,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       'no-empty-pattern': 2,
       'import/no-useless-path-segments': 2,
       'import/export': 2,
@@ -40,12 +32,9 @@ export default tseslint.config(
       'import/no-amd': 2,
       'import/no-commonjs': 2,
       'import/first': 2,
-      'import/exports-last': 0,
       'import/no-duplicates': 2,
-      'import/no-namespace': 0,
       'import/order': 2,
       'import/newline-after-import': 2,
-      'import/prefer-default-export': 0,
     },
   },
 );
